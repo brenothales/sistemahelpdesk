b8012aa871e268328d248c1763aea2f1f1ccbb3d
Breno Thales <breno.thales@gmail.com>
2012-10-24 13:29:25 -0400

    alterando o model de comment para pegar o valor do usuario logado

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :chamado
+  belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
   attr_accessible :descricao, :user_id, :name
 end
